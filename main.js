b07825a99b3151ddf1df2d570fd271e5357ef8ab
martinbarker <martinbarker99@gmail.com>
2020-12-19 13:26:18 -0800

    wip

---
+++ main.js
@@ -0,0 +1,63 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+    mainWindow.loadFile('./src/index.html');
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+
+    // Open the DevTools. 
+    mainWindow.webContents.openDevTools()
+
+    // check if there are any updates availiable once main window is ready. if there are, automatically download 
+    mainWindow.once('ready-to-show', () => {
+        autoUpdater.checkForUpdatesAndNotify();
+    });
+
+}
+
+app.on('ready', () => {
+    createWindow();
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+//send app version to main window
+ipcMain.on('app_version', (event) => {
+    event.sender.send('app_version', { version: app.getVersion() });
+});
+
+//auto-update quiet and install
+ipcMain.on('restart_app', () => {
+    autoUpdater.quitAndInstall();
+});
+
+//handle auto-update events
+autoUpdater.on('update-available', () => {
+    mainWindow.webContents.send('update_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+    mainWindow.webContents.send('update_downloaded');
+});
---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+const { ipcRenderer } = require('electron');
+const version = document.getElementById('version');
+
+/*
+  auto-update popup downloading code
+*/
+const notification = document.getElementById('notification');
+const message = document.getElementById('message');
+const restartButton = document.getElementById('restart-button');
+ipcRenderer.on('update_available', () => {
+  ipcRenderer.removeAllListeners('update_available');
+  message.innerText = 'A new update is available. Downloading now...';
+  notification.classList.remove('hidden');
+});
+ipcRenderer.on('update_downloaded', () => {
+  ipcRenderer.removeAllListeners('update_downloaded');
+  message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
+  restartButton.classList.remove('hidden');
+  notification.classList.remove('hidden');
+});
+function closeNotification() {
+  notification.classList.add('hidden');
+}
+function restartApp() {
+  ipcRenderer.send('restart_app');
+}
+
+/*
+  recieve app version and send it to html 
+*/
+ipcRenderer.send('app_version');
+ipcRenderer.on('app_version', (event, arg) => {
+  ipcRenderer.removeAllListeners('app_version');
+  version.innerText = 'Version ' + arg.version;
+});
