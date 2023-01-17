fa3b66ad5e344626ab4c200cc41d4893d77e3022
martinbarker99 <martinbarker99@gmail.com>
2023-01-16 20:28:35 -0800

    icons prep

--- src/App.js
+++ src/App.js
@@ -7,13 +7,18 @@ import Projects from './Projects/ProjectsClass'
 import Sidebar from './Sidebar/Sidebar'
 
 /*
+render2une: free open-source video rendering ffmpeg wrapper for youtube music accounts 
+
 TODO:
 - frameless window
 - expand/collapse responsive sidebar UI (clean code/css)
 - add/edit/delete projects flow
-- ffmpeg process tracking
+- ffmpeg process tracking (track: time, progress, how gpu/cpu/memory intensive the command is )
 - youtube auth+upload
 - discogs auth+edit
+- view render options UI
+  * view/edit raw ffmpeg command with explanations
+  * 
 */
 
 
--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -36,33 +36,46 @@ function Sidebar() {
 
     return (
         <>
+
             <div className="main-content">
 
                 {/* Always Visible Icons Sidebar */}
                 <div id="sidebarIcons">
                     <div>
-                        <a id="homeButton" className="" href="#">
-                            <i className="fa fa-home" aria-hidden="true"></i>
+
+                        {/* home */}
+                        <a id="homeButton" href="#">
+                            <svg className="custom-svg" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1472 992v480q0 26-19 45t-45 19h-384v-384h-256v384h-384q-26 0-45-19t-19-45v-480q0-1 .5-3t.5-3l575-474 575 474q1 2 1 6zm223-69l-62 74q-8 9-21 11h-3q-13 0-21-7l-692-577-692 577q-12 8-24 7-13-2-21-11l-62-74q-8-10-7-23.5t11-21.5l719-599q32-26 76-26t76 26l244 204v-195q0-14 9-23t23-9h192q14 0 23 9t9 23v408l219 182q10 8 11 21.5t-7 23.5z" />
+                            </svg>
                         </a>
-                        <a data-toggle="modal" data-target="#new-upload-modal" id="newUploadButton" href="#" className="">
-                            <i href="#" className="fa fa-plus-circle" aria-hidden="true"></i>
+
+                        {/* plus sign in circle (new project) */}
+                        <a data-toggle="modal" data-target="#new-upload-modal" id="newUploadButton" href="#" >
+                            <svg className="custom-svg" width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1344 960v-128q0-26-19-45t-45-19h-256v-256q0-26-19-45t-45-19h-128q-26 0-45 19t-19 45v256h-256q-26 0-45 19t-19 45v128q0 26 19 45t45 19h256v256q0 26 19 45t45 19h128q26 0 45-19t19-45v-256h256q26 0 45-19t19-45zm320-64q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z" />
+                            </svg>
                         </a>
-                        {/*
-                        <a href="#menu-toggle" id="menu-toggle" className="svg-selected">
-                            <svg className="custom-svg" xmlns="http://www.w3.org/2000/svg" version="1.0" width="168.000000pt" height="149.000000pt" viewBox="0 0 168.000000 149.000000" preserveAspectRatio="xMidYMid meet">
-                                <g className="custom-svg" transform="translate(0.000000,149.000000) scale(0.100000,-0.100000)" fill="#818181" stroke="none">
+
+
+
+
+
+                        <a href="#menu-toggle" id="menu-toggle" >
+                            <svg className="custom-svg" xmlns="http://www.w3.org/2000/svg" version="1.0" viewBox="0 0 168.000000 149.000000" preserveAspectRatio="xMidYMid meet">
+                                <g transform="translate(0.000000,149.000000) scale(0.100000,-0.100000)" fill="#818181" stroke="none">
                                     <path d="M1045 1433 c-88 -29 -168 -56 -177 -59 -16 -5 -18 2 -18 45 l0 51 -190 0 -190 0 0 -730 0 -730 190 0 190 0 0 677 c0 373 4 673 9 668 4 -6 42 -111 84 -235 91 -273 379 -1114 381 -1117 1 -1 81 23 178 54 l176 56 -135 396 c-74 218 -177 522 -230 676 -52 154 -98 285 -102 291 -4 8 -58 -6 -166 -43z"></path>
                                     <path d="M0 740 l0 -730 190 0 190 0 0 730 0 730 -190 0 -190 0 0 -730z"></path>
                                 </g>
                             </svg>
                         </a>
-                        <a data-toggle="modal" data-target="#render-jobs-modal" id="renderJobsButton" href="#" className="">
-                            <i className="fa fa-tasks" aria-hidden="true"></i>
-                            <div className="spinner-border renderJobsIconCircle" role="status" >
-                                <span className="sr-only">Loading...</span>
-                            </div>
+
+                        <a href="#menu-toggle" id="menu-toggle" >
+                            <svg className="custom-svg" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M256 1312v192q0 13-9.5 22.5t-22.5 9.5h-192q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h192q13 0 22.5 9.5t9.5 22.5zm0-384v192q0 13-9.5 22.5t-22.5 9.5h-192q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h192q13 0 22.5 9.5t9.5 22.5zm0-384v192q0 13-9.5 22.5t-22.5 9.5h-192q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h192q13 0 22.5 9.5t9.5 22.5zm1536 768v192q0 13-9.5 22.5t-22.5 9.5h-1344q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h1344q13 0 22.5 9.5t9.5 22.5zm-1536-1152v192q0 13-9.5 22.5t-22.5 9.5h-192q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h192q13 0 22.5 9.5t9.5 22.5zm1536 768v192q0 13-9.5 22.5t-22.5 9.5h-1344q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h1344q13 0 22.5 9.5t9.5 22.5zm0-384v192q0 13-9.5 22.5t-22.5 9.5h-1344q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h1344q13 0 22.5 9.5t9.5 22.5zm0-384v192q0 13-9.5 22.5t-22.5 9.5h-1344q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h1344q13 0 22.5 9.5t9.5 22.5z" /></svg>
                         </a>
-                    */}
+
+
+                        {/* */}
                     </div>
                 </div>
 
