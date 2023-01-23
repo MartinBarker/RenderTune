2686cef7d919ea87299641b2ef785b73fdcb3ef8
unknown <martinbarker99@gmail.com>
2023-01-22 17:29:41 -0800

    wip close feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import React from 'react';
-import './FrameStyle.css'
 
 import Border from './Border/Border'
 import FfmpegTest from './FfmpegTest'
@@ -28,51 +27,19 @@ TODO:
 
 function App() {
 
-  const remote = require('electron').remote;
-
-  const win = remote.getCurrentWindow();
+  //border.js stuff:
+  //const remote = require('electron').remote;
+  //const win = remote.getCurrentWindow();
 
   return (
-        <div id='frameWrapper'>
-          <header id="titlebar">
-            <div id="drag-region">
-              <span>Electron quick start</span>
-              <div id="window-controls">
-
-                <div className="button" id="min-button">
-                  <img className="icon" srcSet="icons/min-w-10.png 1x, icons/min-w-12.png 1.25x, icons/min-w-15.png 1.5x, icons/min-w-15.png 1.75x, icons/min-w-20.png 2x, icons/min-w-20.png 2.25x, icons/min-w-24.png 2.5x, icons/min-w-30.png 3x, icons/min-w-30.png 3.5x" draggable="false" />
-                </div>
-
-                <div className="button" id="max-button">
-                  <img className="icon" srcSet="icons/max-w-10.png 1x, icons/max-w-12.png 1.25x, icons/max-w-15.png 1.5x, icons/max-w-15.png 1.75x, icons/max-w-20.png 2x, icons/max-w-20.png 2.25x, icons/max-w-24.png 2.5x, icons/max-w-30.png 3x, icons/max-w-30.png 3.5x" draggable="false" />
-                </div>
-
-                <div className="button" id="restore-button">
-                  <img className="icon" srcSet="icons/restore-w-10.png 1x, icons/restore-w-12.png 1.25x, icons/restore-w-15.png 1.5x, icons/restore-w-15.png 1.75x, icons/restore-w-20.png 2x, icons/restore-w-20.png 2.25x, icons/restore-w-24.png 2.5x, icons/restore-w-30.png 3x, icons/restore-w-30.png 3.5x" draggable="false" />
-                </div>
-
-                <div className="button" id="close-button">
-                  <img className="icon" srcSet="icons/close-w-10.png 1x, icons/close-w-12.png 1.25x, icons/close-w-15.png 1.5x, icons/close-w-15.png 1.75x, icons/close-w-20.png 2x, icons/close-w-20.png 2.25x, icons/close-w-24.png 2.5x, icons/close-w-30.png 3x, icons/close-w-30.png 3.5x" draggable="false" />
-                </div>
-
-              </div>
-            </div>
-      </header>
-
-    <div id="main">
-    <h1>Hello World!</h1>
-    <p>Lorem ipsum dolor sit amet...</p>
-  </div>
-
+    <>
+    <Border/>
       {/*
-      <Border/>
-      <FfmpegTest/>
+    <FfmpegTest/>
       <Projects/>
-
       <Sidebar/>
-    
       */}
-      </div >
+    </>
 
   );
 }
--- src/Border/Border.js
+++ src/Border/Border.js
@@ -45,14 +45,51 @@ function Border() {
   return (
     <div>
 
-      BORDER {appVersion}
+
+      <div id='frameWrapper'>
+        <header id="titlebar">
+          <div id="drag-region">
+            <span>RenderTune v{appVersion}</span>
+            <div id="window-controls">
+
+              <div className="button" id="min-button">
+              
+                <svg draggable="false" className="" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1472 992v480q0 26-19 45t-45 19h-384v-384h-256v384h-384q-26 0-45-19t-19-45v-480q0-1 .5-3t.5-3l575-474 575 474q1 2 1 6zm223-69l-62 74q-8 9-21 11h-3q-13 0-21-7l-692-577-692 577q-12 8-24 7-13-2-21-11l-62-74q-8-10-7-23.5t11-21.5l719-599q32-26 76-26t76 26l244 204v-195q0-14 9-23t23-9h192q14 0 23 9t9 23v408l219 182q10 8 11 21.5t-7 23.5z" />
+                </svg>
+
+              </div>
+
+              <div className="button" id="max-button">
+                <img className="icon" srcSet="./icons/max-w-10.png 1x, ./icons/max-w-12.png 1.25x, ./icons/max-w-15.png 1.5x, ./icons/max-w-15.png 1.75x, ./icons/max-w-20.png 2x, ./icons/max-w-20.png 2.25x, ./icons/max-w-24.png 2.5x, ./icons/max-w-30.png 3x, ./icons/max-w-30.png 3.5x" draggable="false" />
+              </div>
+
+              <div className="button" id="restore-button">
+                <img className="icon" srcSet="./icons/restore-w-10.png 1x, ./icons/restore-w-12.png 1.25x, ./icons/restore-w-15.png 1.5x, ./icons/restore-w-15.png 1.75x, ./icons/restore-w-20.png 2x, ./icons/restore-w-20.png 2.25x, ./icons/restore-w-24.png 2.5x, ./icons/restore-w-30.png 3x, ./icons/restore-w-30.png 3.5x" draggable="false" />
+              </div>
+
+              <div className="button" id="close-button">
+                <img className="icon" srcSet="./icons/close-w-10.png 1x, ./icons/close-w-12.png 1.25x, ./icons/close-w-15.png 1.5x, ./icons/close-w-15.png 1.75x, ./icons/close-w-20.png 2x, ./icons/close-w-20.png 2.25x, ./icons/close-w-24.png 2.5x, ./icons/close-w-30.png 3x, ./icons/close-w-30.png 3.5x" draggable="false" />
+              </div>
+
+            </div>
+          </div>
+        </header>
+        <div id="main">
+          <h1>Hello World!</h1>
+          <p>Lorem ipsum dolor sit amet...</p>
+          <p>Lorem ipsum dolor sit amet...</p>
+          <p>Lorem ipsum dolor sit amet...</p>
+          <p>Lorem ipsum dolor sit amet...</p>
+        </div>
+      </div >
 
       <div id="notification" className="hidden">
         <p id="message">Update status: {appUpdateStatus}</p>
         <button id="close-button" onClick={closeNotification}>
           Close
         </button>
-        <button id="restart-button" onClick={restartApp} className={ restartApp='restart' ? "" : "hidden"}>
+        <button id="restart-button" onClick={restartApp} className={restartApp = 'restart' ? "" : "hidden"}>
           Restart
         </button>
       </div>
