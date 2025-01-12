93ad4f0a2f979e65930e44d19f43da2957991884
Martin Barker <martinbarker99@gmail.com>
2025-01-11 18:31:36 -0800

    react_dev_init

---
+++ preload.js
@@ -0,0 +1,39 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+    send: (channel, data) => {
+        const validSendChannels = [
+            'app_version', 
+            'minimize-window', 
+            'maximize-window', 
+            'unmaximize-window', 
+            'close-window',
+            'run-ffmpeg-command',
+            'get-audio-metadata',  
+            'open-file-dialog',
+            'open-folder-dialog',
+            'get-path-separator'
+        ];
+        if (validSendChannels.includes(channel)) {
+            ipcRenderer.send(channel, data);
+        }
+    },
+    receive: (channel, func) => {
+        const validReceiveChannels = [
+            'app_version',
+            'ffmpeg-output', 
+            'ffmpeg-error',
+            'audio-metadata-response', 
+            'selected-file-paths',
+            'selected-folder',
+            'path-separator-response',
+            'ffmpeg-progress'    
+        ];
+        if (validReceiveChannels.includes(channel)) {
+            ipcRenderer.on(channel, (event, ...args) => func(...args));
+        }
+    },
+    removeAllListeners: (channel) => {
+        ipcRenderer.removeAllListeners(channel);
+    }
+});
---
+++ src/Settings/Settings.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function Settings() {
+    return (
+        <>
+            Settings Page
+        </>
+    );
+}
+
+export default Settings;
