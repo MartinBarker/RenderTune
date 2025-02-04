3d169361ccdc5b25d772babecad7bbe3feeaa8f6
Martin Barker <martinbarker99@gmail.com>
2025-02-03 21:11:06 -0800

    new version sidebar org

---
+++ src/Bug/Bug.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import {
+    HashRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+
+function Bug() {
+    return (
+        <>
+            <h1>Bug Report</h1>
+        </>
+    );
+}
+
+export default Bug;
---
+++ src/YouTube/YouTube.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import {
+    HashRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+
+
+function YouTube() {
+    return (
+        <>
+            <h1>YouTube Page Content</h1>
+            <p>This is a work-in-progress page and will feature auto-uploading soon.</p>
+        </>
+    );
+}
+
+export default YouTube;
